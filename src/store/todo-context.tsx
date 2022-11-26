c2775e3863472e1890ef455a24348ce36921aacc
Iqbal Nugraha <iqbalnugraha347@gmail.com>
2022-11-27 00:04:15 +0700

    fix: fixing error on todos context

--- src/store/todo-context.tsx
+++ src/store/todo-context.tsx
@@ -3,9 +3,13 @@ import { TodoContextObj } from "../types";
 
 const TodosContext = React.createContext<TodoContextObj>({
   items: [],
+  filterType: "",
+  setTodos: (e: any) => {},
   addTodoHandler: (title: string) => {},
   removeTodoHandler: (id: number) => {},
-  clearTodoCompleteHandler: (id: number[]) => {},
+  clearTodoCompleteHandler: () => {},
+  checkTodoHandler: (id: number) => {},
+  changeTypeFilter: (type: string) => {},
 });
 
 export default TodosContext;
